3ff7afc19489ac47b47fa853b70a4366524ecf7e
fagnerviana <fagsantiago@gmail.com>
2023-04-24 16:06:26 -0300

    Finalizado o metodo Update usuario passando o ID

--- src/main/java/application/Teste.java
+++ src/main/java/application/Teste.java
@@ -8,6 +8,35 @@ public class Teste {
 
 	public static void main(String[] args) throws ClassNotFoundException {
 		
+		
+		//Testando o metodo update UsuarioDao inserindo o seu ID:
+		Usuario usuario = new Usuario();
+		usuario.setNome("Pelé");
+		usuario.setSenha("123456789");
+		usuario.setLogin("reipele");
+		usuario.setTipoUsuario(TipoUsuario.Professor);
+		usuario.setId(1);
+		
+		
+		UsuarioDao usuarioDao = new UsuarioDao();
+		usuarioDao.updateUsuario(usuario);
+
+		
+		
+		/*
+		//Testando o metodo update UsuarioDao inserindo o seu ID:
+				Usuario usuario = new Usuario();
+				usuario.setNome("Pelé");
+				usuario.setSenha("123456789");
+				usuario.setLogin("reipele");
+				usuario.setTipoUsuario(TipoUsuario.Professor);
+				usuario.setId(1);
+				
+				
+				UsuarioDao usuarioDao = new UsuarioDao();
+				usuarioDao.updateUsuario(usuario);
+		
+				
 		//Testando o modelo ususario com insert no banco de dados:
 		Usuario usuario = new Usuario();
 		usuario.setNome("Homem Aranha");
@@ -17,7 +46,7 @@ public class Teste {
 		
 		UsuarioDao usuarioDao = new UsuarioDao();
 		usuarioDao.insertUsuario(usuario);
-
+		*/
 	}
 
 }
--- src/main/java/persistencia/UsuarioDao.java
+++ src/main/java/persistencia/UsuarioDao.java
@@ -11,16 +11,17 @@ public class UsuarioDao {
 	private Connection connection;
 
 	public UsuarioDao() {
-	    try {
-	      connection = Conecta.criarConexao();
-	    } catch (ClassNotFoundException e) {
-	      e.printStackTrace();
-	    } catch (SQLException e) {
-	      e.printStackTrace();
-	    }
-	  }
+		try {
+			connection = Conecta.criarConexao();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 	// Ok funcionando
+	// Realiza a inserção no banco de dados do objeto Usuario
 	public void insertUsuario(Usuario usuario) throws ClassNotFoundException {
 		try {
 			PreparedStatement statement = connection
@@ -39,41 +40,56 @@ public class UsuarioDao {
 		}
 	}
 
+	public void updateUsuario(Usuario usuario) { 
+	 
+	try { 
+		PreparedStatement statement = connection.prepareStatement(
+	  "UPDATE usuario SET nome=?,login=?,senha=?,tipousuario=? WHERE id_ususario=?");
+	  statement.setString(1,usuario.getNome()); 
+	  statement.setString(2,usuario.getSenha()); 
+	  statement.setString(3,usuario.getLogin());
+	  statement.setString(4,usuario.getTipoUsuario().getDescricao());
+	  statement.setInt(5, usuario.getId());
+	  
+	  statement.execute(); 
+	  statement.close(); } 
+	catch (SQLException e) {
+	  e.printStackTrace(); 
+	  }
+	}
+
 	/*
-	 * public void updatePessoa(Pessoa pessoa) { try { PreparedStatement statement =
-	 * connection.prepareStatement(
-	 * "UPDATE pessoa SET nome = ?,email=?,renda=? WHERE id_pessoa=?");
-	 * //statement.setInt(1,pessoa.getId()); statement.setString(1,pessoa.getNome()
-	 * ); statement.setString(2,pessoa.getEmail()); statement.setDouble(3,
-	 * pessoa.getRenda()); statement.setInt(4, pessoa.getId());
-	 * 
-	 * statement.execute(); statement.close(); } catch (SQLException e) {
-	 * e.printStackTrace(); } }
-	 * 
-	 * public void deletePessoa(int id) { try { PreparedStatement statement =
-	 * connection.prepareStatement( "DELETE from pessoa WHERE id_pessoa=?");
-	 * statement.setInt(1,id); statement.executeUpdate(); } catch (SQLException e) {
-	 * e.printStackTrace(); } }
-	 * 
-	 * //Resolver Depois descobrir como realizar pesquisa no bd pelo ENUM public
-	 * Pessoa getPessoaById(int id) { Pessoa pessoa =null; TipoRenda renda = null;
-	 * 
-	 * try { PreparedStatement statement = connection.prepareStatement(
-	 * "SELECT *FROM pessoa WHERE id_pessoa = ?"); statement.setInt(1, id);
-	 * ResultSet resultSet = statement.executeQuery(); if (resultSet.next()) {
-	 * 
-	 * 
-	 * pessoa = new Pessoa(); pessoa.setId(resultSet.getInt("id_pessoa"));
-	 * pessoa.setNome(resultSet.getString("nome"));
-	 * pessoa.setEmail(resultSet.getString("email"));
-	 * pessoa.setRenda(resultSet.getDouble("renda"));
-	 * //pessoa.setTiporenda(TipoRenda.values().hashCode(TipoRenda.getTipoRendaInt(
-	 * resultSet.getInt("renda")));
-	 * //pessoa.se(TipoRenda.getTipoRendaInt(resultSet.getInt("renda")));
-	 * pessoa.setTiporenda(TipoRenda.getBy(resultSet.getInt("id_tiporenda")));
-	 * pessoa.setDependentes(resultSet.getInt("dependentes"));
-	 * 
-	 * } } catch (SQLException e) { e.printStackTrace(); } return pessoa; }
+	
+	public void deletePessoa(int id) {
+		try {
+			PreparedStatement statement = connection.prepareStatement("DELETE from pessoa WHERE id_pessoa=?");
+			statement.setInt(1, id);
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// Resolver Depois descobrir como realizar pesquisa no bd pelo ENUM public
+	Pessoa getPessoaById(int id) { Pessoa pessoa =null; TipoRenda renda = null;
+	  
+	  try { PreparedStatement statement = connection.prepareStatement(
+	  "SELECT *FROM pessoa WHERE id_pessoa = ?"); statement.setInt(1, id);
+	  ResultSet resultSet = statement.executeQuery(); if (resultSet.next()) {
+	  
+	  
+	  pessoa = new Pessoa(); pessoa.setId(resultSet.getInt("id_pessoa"));
+	  pessoa.setNome(resultSet.getString("nome"));
+	  pessoa.setEmail(resultSet.getString("email"));
+	  pessoa.setRenda(resultSet.getDouble("renda"));
+	  //pessoa.setTiporenda(TipoRenda.values().hashCode(TipoRenda.getTipoRendaInt(
+	  resultSet.getInt("renda")));
+	  //pessoa.se(TipoRenda.getTipoRendaInt(resultSet.getInt("renda")));
+	  pessoa.setTiporenda(TipoRenda.getBy(resultSet.getInt("id_tiporenda")));
+	  pessoa.setDependentes(resultSet.getInt("dependentes"));
+	  
+	 } } catch (SQLException e) { e.printStackTrace(); } return pessoa; }
+	 
 	 */
 
 }
